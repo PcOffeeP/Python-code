18719aee372fd6bfc1a9835ba7d0fdbedc236bab
PcOffeeP <15033039327@163.com>
2026-09-17 00:43:36 +0800

    Initialize project structure with .gitignore, .python-version, README.md, and pyproject.toml; implement K-means clustering functions in bi-Kmean.py

---
+++ bi-Kmean.py
@@ -0,0 +1,47 @@
+
+def dist2(p1:list, p2:list) -> float:
+    '''
+    计算两点之间的距离。
+    '''
+    return (p1[0] - p2[0]) **2 + (p1[1] - p2[1]) **2
+
+def centroid(points:list) -> tuple:
+    l = len(points)
+    cx = sum(x for x, y in points) / l
+    cy = sum(y for x, y in points) / l
+    return cx, cy
+
+def calc_sse(points):
+    center = centroid(points)
+    sse = 0
+
+    for point in points:
+        sse += dist2(point, center)
+
+    return sse
+
+def split_cluster(points):
+    c1 = max(points, key=lambda p: p[0])
+    c2 = min(points, key=lambda p: p[0])
+
+    cluster0 = []
+    cluster1 = []
+    tag = []
+
+    for point in points:
+        d1 = dist2(point, c1)
+        d2 = dist2(point, c2)
+
+        if d1 < d2:
+            cluster0.append(point)
+            tag.append(0)
+        else:
+            cluster1.append(point)
+            tag.append(1)
+
+    new_c1 = centroid(cluster0)
+    new_c2 = centroid(cluster1)
+
+    c1 = new_c1
+    c2 = new_c2
+
